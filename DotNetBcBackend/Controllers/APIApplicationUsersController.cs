d84f86f1433114479f761a20ecda9fb291a054ec
Janice Chan <gaga.0929@gmail.com>
2017-04-07 16:42:20 -0700

    added api for users

---
+++ DotNetBcBackend/Controllers/APIApplicationUsersController.cs
@@ -0,0 +1,128 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DotNetBcBackend.Data;
+using DotNetBcBackend.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace DotNetBcBackend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/APIApplicationUsers")]
+    [Authorize]
+    public class APIApplicationUsersController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public APIApplicationUsersController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/APIApplicationUsers
+        [HttpGet]
+        public IEnumerable<ApplicationUser> GetApplicationUsers()
+        {
+            return _context.ApplicationUsers;
+        }
+
+        // GET: api/APIApplicationUsers/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetApplicationUser([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.Id == id);
+
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(applicationUser);
+        }
+
+        // PUT: api/APIApplicationUsers/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutApplicationUser([FromRoute] string id, [FromBody] ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != applicationUser.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(applicationUser).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ApplicationUserExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/APIApplicationUsers
+        [HttpPost]
+        public async Task<IActionResult> PostApplicationUser([FromBody] ApplicationUser applicationUser)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.ApplicationUsers.Add(applicationUser);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetApplicationUser", new { id = applicationUser.Id }, applicationUser);
+        }
+
+        // DELETE: api/APIApplicationUsers/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteApplicationUser([FromRoute] string id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.Id == id);
+            if (applicationUser == null)
+            {
+                return NotFound();
+            }
+
+            _context.ApplicationUsers.Remove(applicationUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(applicationUser);
+        }
+
+        private bool ApplicationUserExists(string id)
+        {
+            return _context.ApplicationUsers.Any(e => e.Id == id);
+        }
+    }
+}
