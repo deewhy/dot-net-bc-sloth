6af430a4f29b1389a78eae9b4c30e89bea8fd96b
Renato Montes <renatomontes36@gmail.com>
2017-04-10 10:15:33 -0700

    added PUT and POST methods for AttendEvents Web API

--- DotNetBcBackend/Controllers/APIAttendEvents.cs
+++ DotNetBcBackend/Controllers/APIAttendEvents.cs
@@ -28,8 +28,7 @@ namespace DotNetBcBackend.Controllers
             _userManager = userManager;
         }
 
-        // GET: api/APIApplicationUsers
-        //returns First Name, Last Name, email, and city of ApplicationUser
+        // GET: api/APIAttendEvents
         [HttpGet]
         public async Task<IActionResult> GetExistsAttending([FromHeader] string UserName, [FromHeader] string Password, [FromHeader] string Evid)
         {
@@ -42,33 +41,137 @@ namespace DotNetBcBackend.Controllers
 
             if (applicationUser == null)
             {
-                return BadRequest(new { Error = "1" });
+                return BadRequest();
             }
             
-            /*
             if (!await _userManager.CheckPasswordAsync(applicationUser, Password))
             {
-                return BadRequest(new { Error = "2" });
+                return BadRequest();
             }
-            */
             
             var myEvent = await _context.Events.SingleOrDefaultAsync(m => m.Evid == Convert.ToInt64(Evid));
             
             if (myEvent == null)
             {
-                return BadRequest(new { Error = "3" });
+                return BadRequest();
             }
 
-            var UserEvent = await _context.UserEvents.SingleOrDefaultAsync(ue => ue.Userid == applicationUser.Id && ue.Evid == myEvent.Evid);
+            var UserEvent = await _context.UserEvents.SingleOrDefaultAsync(ue => ue.Userid == applicationUser.Id && ue.Evid == Convert.ToInt64(Evid));
 
             if (UserEvent == null)
             {
                 return Ok(new { Exists = false, Attending = false });
             }
 
-            return Ok(new { Exists = true, Attending = UserEvent.Attending });
+            if (UserEvent.Attending == true)
+            {
+                return Ok(new { Exists = true, Attending = true });
+            } else
+            {
+                return Ok(new { Exists = true, Attending = false });
+            }
         }
 
+        // POST: api/APIAttendEvents
+        [HttpPost]
+        public async Task<IActionResult> PostUserEvent([FromBody] APIAttendEventsModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
 
+            if (model == null)
+            {
+                return BadRequest(new { Error = "JSON object was found to be null." });
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.UserName == model.UserName);
+
+            if (applicationUser == null)
+            {
+                return BadRequest();
+            }
+
+            if (!await _userManager.CheckPasswordAsync(applicationUser, model.Password))
+            {
+                return BadRequest();
+            }
+
+            var newUserEvent = new UserEvents
+            {
+                Evid = model.Evid,
+                Userid = applicationUser.Id,
+                Attending = true
+            };
+
+            _context.Entry<UserEvents>(newUserEvent).State = EntityState.Added;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            } catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+
+        // PUT: api/AttendEvents
+        [HttpPut]
+        public async Task<IActionResult> PutUserEvent([FromBody] APIAttendEventsModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (model == null)
+            {
+                return BadRequest(new { Error = "JSON object was found to be null." });
+            }
+
+            var applicationUser = await _context.ApplicationUsers.SingleOrDefaultAsync(m => m.UserName == model.UserName);
+
+            if (applicationUser == null)
+            {
+                return BadRequest();
+            }
+
+            if (!await _userManager.CheckPasswordAsync(applicationUser, model.Password))
+            {
+                return BadRequest();
+            }
+
+            var myEvent = await _context.Events.SingleOrDefaultAsync(m => m.Evid == model.Evid);
+
+            if (myEvent == null)
+            {
+                return BadRequest();
+            }
+
+            var UserEvent = await _context.UserEvents.SingleOrDefaultAsync(ue => ue.Userid == applicationUser.Id && ue.Evid == myEvent.Evid);
+
+            if (UserEvent == null)
+            {
+                return BadRequest();
+            }
+
+            UserEvent.Attending = model.Attending;
+
+            _context.Entry(UserEvent).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
     }
 }
